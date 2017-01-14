900f5555dcaa6de7a8fabf2df33d9d7235cc1efc
Shazahanul Islam Shohag <shohag121@gmail.com>
2017-01-15 02:30:14 +0600

    working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+  constructor(public af: AngularFire){
+    this.items = af.database.list('/messages',{
+      query: {
+        limitToLast: 5
+      }
+    });
+
+    this.af.auth.subscribe(auth => {
+      if(auth){
+        this.name = auth;
+      }
+    });
+
+
+  }
+
+  login(){
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup
+    });
+  }
+
+  chatSend(thisMessage:string){
+    this.items.push({message: thisMessage, name: this.name.facebook.fullName});
+    this.msgVal = '';
+  }
+
+  logout(){
+    this.af.auth.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig ={
+  apiKey: "AIzaSyDfSyiwcBet2sSFofKTqP5gHk8qJPqKRMc",
+  authDomain: "chitchat-c6246.firebaseapp.com",
+  databaseURL: "https://chitchat-c6246.firebaseio.com",
+  storageBucket: "chitchat-c6246.appspot.com",
+  messagingSenderId: "988620228128"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
